376a66fbc830bb54cdfc09be9af2df33ef8ce8d1
Mustapha Babatunde <toystars2008@gmail.com>
2015-11-28 17:45:22 +0100

    Implement add, addAll, toString, toArray, size, and isList functions of eList

--- src/list.js
+++ src/list.js
@@ -9,6 +9,15 @@
 
 var eList = function() {
 
+  /**
+    * Define all utility functions here...
+    */
+  var isObjectOf = function(object) {
+    if (typeof object !== 'object' || !object.hasOwnProperty('eList')) {
+      return false;
+    }
+  };
+
   // object containing all properties and functions of the list
   var list = {};
 
@@ -33,46 +42,128 @@ var eList = function() {
   /**
     * Count property to be used to get quick number of elements in the list
     */
-  list.count = 0;
+  list.size = function() {
+    return list.data.length;
+  };
 
   /**
     * Adds an element to the list.
     * @param {Integer} index - Optional index to add the element. If no index is specified
     * the element is added to the end of the list.
     * @param {Object} item - Element to be added (must always be present).
-    * @return {boolean} True if the element was added or false if error occurs
+    * @return {Integer} Non-negeative integer if the element was added or -1 if error occurs
     */
   list.add = function(index, item) {
     // if arguments.length === 0 || > 2, throw error
     if (arguments.length === 0 || arguments.length > 2) {
-      return false;
+      return -1;
     }
 
     // if arguments.length === 1, treat as element to be inserted and insert as last element of list
     if (arguments.length === 1) {
       list.data.push(arguments[0]);
-      return true;
+      return list.size();
     }
 
     // if arguments.length === 2, treat first as index and second as item
     if (arguments.length === 2) {
       // check if specified index is a number
       if ((typeof index !== 'number') || (index % 1 !== 0) || (index < 0)) {
-        return false;
+        return -1;
       }
 
       // all checks passed, insert into list
       if (list.data.length - 1 < index) {
         // push into array
         list.data.push(item);
-        return true;
+        return list.size();
       } else if (index < list.data.length - 1) {
-        // spliice array and insert item
+        // splice array and insert item
         list.data.splice(index, 0, item);
-        return true;
+        return list.size();
       }
     }
-    return false;
+    return -1;
+  };
+
+  /**
+    * Adds another eList to the list.
+    * @param {Integer} index - Optional index to add the eList. If no index is specified
+    * the eList is added to the end of the list.
+    * @param {Object} eList - List to be added (must always be present).
+    * @return {Integer} Non-negeative integer if the eList was added or -1 if error occurs
+    */
+  list.addAll = function(index, eList) {
+    // if arguments.length === 0 || > 2, throw error
+    if (arguments.length === 0 || arguments.length > 2) {
+      return -1;
+    }
+
+    // if arguments.length === 1, treat as eList to be inflated and insert as last element of list
+    if (arguments.length === 1) {
+      // check if argument is an eList
+      if (isObjectOf(arguments[0]) === false) {
+        return null;
+      }
+
+      list.data = list.data.concat(arguments[0].toArray());
+      return list.size();
+    }
+
+    // if arguments.length === 2, treat first as index and second as item
+    if (arguments.length === 2) {
+      // check if specified index is a number
+      if ((typeof index !== 'number') || (index % 1 !== 0) || (index < 0)) {
+        return -1;
+      }
+
+      // check if argument is an eList
+      if (isObjectOf(eList) === false) {
+        return null;
+      }
+
+      // all checks passed, insert into list
+      if (list.data.length - 1 < index) {
+        // append new eList to current
+        list.data = list.data.concat(eList.toArray());
+        return list.size();
+      } else if (index < list.data.length - 1) {
+        // go through array and splice at incrementing index
+        var currentIndex = index;
+        for (var x = 0; x < eList.size(); x++) {
+          list.data.splice(currentIndex, 0, eList.get(x));
+          currentIndex++
+        }
+        return list.size();
+      }
+    }
+    return -1;
+  };
+
+  /**
+    * Retrieve an element to the list.
+    * @param {Integer} index - index of element to be retrieved
+    * @return {Object} Object at index if found, -1 if error occur
+    */
+  list.get = function(index) {
+    if (index === undefined || index < 0 || index >= list.size()) {
+      return -1;
+    }
+    return list.data[index];
+  };
+
+  /**
+    * @return {Array} Array containing all eList elements or empty array if eList is empty
+    */
+  list.toArray = function() {
+    return list.data;
+  };
+
+  /**
+    * @return {String} String containing all eList elements or empty string if eList is empty
+    */
+  list.toString = function() {
+    return list.data.toString();
   };
 
   return list;
--- tests/list-test.js
+++ tests/list-test.js
@@ -5,39 +5,132 @@
   * Test cases for eList data structure
   */
 
-describe("List - isList query", function() {
+describe("eList - isList query", function() {
   it("should return true when called", function() {
     var list = new eList();
     expect(list.isList()).toBe(true);
   });
 });
 
-describe("List - add with no argument", function () {
-  it("should return false when no argument is specified", function () {
+describe("eList - add with no argument", function () {
+  it("should return -1 when no argument is specified", function () {
     var list = new eList();
-    expect(list.add()).toBe(false);
+    expect(list.add()).toBe(-1);
   });
 });
 
-describe("List - add without index", function () {
-  it("should return true when element is inserted", function () {
-    var list = new eList();
-    expect(list.add(5)).toBe(true);
-  });
-});
-
-describe("List - add with index and item", function() {
+describe("eList - add without index", function () {
   var list = new eList();
-  it ("should return false when non-number is used as index", function() {
-    expect(list.add("name", 5)).toBe(false);
+  list.add(5);
+  list.add(22);
+  list.add("eList");
+  it("should return the size of eList when element is inserted", function () {
+    expect(list.add(true)).toBe(4);
   });
-  it ("should return false when non-integer is used as index", function() {
-    expect(list.add(1.7384, 5)).toBe(false);
+});
+
+describe("eList - add with index and item", function() {
+  var list = new eList();
+  it ("should return -1 when non-number is used as index", function() {
+    expect(list.add("name", 5)).toBe(-1);
   });
-  it ("should return false when negative integer is used as index", function() {
-    expect(list.add(-1, 5)).toBe(false);
+  it ("should return -1 when non-integer is used as index", function() {
+    expect(list.add(1.7384, 5)).toBe(-1);
   });
-  it("should return true when element is inserted", function() {
-    expect(list.add(5, 5)).toBe(true);
+  it ("should return -1 when negative integer is used as index", function() {
+    expect(list.add(-1, 5)).toBe(-1);
+  });
+  it("should return eList size when element is inserted", function() {
+    expect(list.add(5, 5)).toBe(1);
+  });
+});
+
+describe("eList - get size of list", function () {
+  it("should return the number of elements in list", function () {
+    var list = new eList();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    expect(list.size()).toBe(3);
+  });
+});
+
+describe("eList - get array representation of the list", function () {
+  var list = new eList();
+  list.add(5);
+  list.add(22);
+  list.add("eList");
+  it("should return an array matching the eList", function () {
+    expect(list.toArray().toString()).toBe([5, 22, "eList"].toString());
+  });
+});
+
+describe("eList - get string representation of the list", function() {
+  var list = new eList();
+  list.add(5);
+  list.add(22);
+  list.add("eList");
+  it("should return an array matching the eList", function () {
+    expect(list.toString()).toBe("5,22,eList");
+  });
+});
+
+describe("eList - get element at specified index", function() {
+  var list = new eList();
+  list.add(5);
+  list.add(22);
+  list.add("eList");
+
+  it("should return -1 when index is not provided", function() {
+    expect(list.get()).toBe(-1);
+  });
+
+  it("should return element in specified index", function () {
+    expect(list.get(2)).toBe("eList");
+  });
+
+  it("should return -1 when index is negative", function() {
+    expect(list.get(-5)).toBe(-1);
+  });
+
+  it("should return -1 when index is greater than all available index", function() {
+    expect(list.get(7)).toBe(-1);
+  });
+});
+
+describe("eList - add another eList object to current eList", function () {
+  var list = new eList();
+  list.add(1);
+  list.add(2);
+  list.add(3);
+  var secondList = new eList();
+  secondList.add("eStructures");
+  secondList.add("JavaScript");
+  secondList.add({name: "eStructures"});
+
+  it("should return null if non-eList object is provided", function() {
+    expect(list.addAll([1, 2, 3, 4])).toBe(null);
+  });    
+
+  it("should return current size of eList after inflating with other eList argument", function () {
+    expect(list.addAll(secondList)).toBe(6);
+  });
+});
+
+describe("eList - add another eList object to current eList at specified index", function () {
+  var list = new eList();
+  list.add(1);
+  list.add(2);
+  list.add(3);
+  var secondList = new eList();
+  secondList.add("eStructures");
+  secondList.add("JavaScript");
+
+  it("should return null if non-eList object is provided", function() {
+    expect(list.addAll(1, [1, 2, 3, 4])).toBe(null);
+  });    
+
+  it("should return current size of eList after inflating with other eList argument", function () {
+    expect(list.addAll(1, secondList)).toBe(5);
   });
 });
