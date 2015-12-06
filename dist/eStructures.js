11c740807e8b60d43905489cfdee5c931d77b434
Mustapha Babatunde <tunde@gaigroup.co>
2015-12-06 12:04:41 +0100

    Implement basic linkedList features

--- dist/eStructures.js
+++ dist/eStructures.js
@@ -1,4 +1,90 @@
 /**
+  * Creates an empty LinkedList.
+  * @class A Linkedlist is a data structure that declares the behavior of a collection 
+  * that stores a sequence of elements with each element pointing to the next element,
+  * while the last element points to null to signify the end of the link chai
+  * @constructor
+  */
+
+var eLinkedList = function () {
+
+  // private references to start and end of linkedList
+  var start = null;
+  var end = null;
+
+  // private length property of linkedList
+  var length = 0;
+
+  // linkedList object to be returned, containing all exposed methods
+  var linkedList = {};
+
+  // function to create empty node
+  var makeNode = function () {
+    return {
+      data: null,
+      next: null
+    };
+  };
+
+  /**
+    * Function to get type of eStructure object
+    */
+  linkedList.getType = function () {
+    return 'eLinkedList';
+  };
+
+  /**
+    * Function to get size of eLinkedList
+    */
+  linkedList.size = function () {
+    return length;
+  };
+
+  /**
+    * Function to get first element of eLinkedList
+    */
+  linkedList.getFirst = function () {
+    return start;
+  };
+
+  /**
+    * Function to get last element of eLinkedList
+    */
+  linkedList.getLast = function () {
+    return end;
+  };
+
+  /**
+    * Adds an element to the linkedList.
+    * @param {Object} item - Element to be added (must always be present).
+    * @return {Integer} Non-negeative integer if the element was added or -1 if error occurs
+    */
+  linkedList.add = function (data) {
+
+    if (!data) {
+      return -1;
+    }
+
+    if (start === null) {
+      start = makeNode();
+      end = start;
+    } else {
+      end.next = makeNode();
+      end = end.next; 
+    }
+
+    end.data = data;
+    length++;
+    return linkedList.size();
+  };
+
+  return linkedList;
+};
+
+if (typeof module !== 'undefined' && module !== null && module.exports) {
+  module.exports = eLinkedList;
+}
+/**
   * Creates an empty List.
   * @class A List is a data structure that declares the behavior of a collection 
   * that stores a sequence of elements.
---
+++ tests/linkedlist-test.js
@@ -0,0 +1,64 @@
+"use strict";
+
+/**
+  * Test cases for eLinkedList data structure
+  */
+
+describe("eLinkedList - getType query", function() {
+  it("should return type of eList", function() {
+    var linkedList = new eLinkedList();
+    expect(linkedList.getType()).toBe('eLinkedList');
+  });
+});
+
+describe("eLinkedList - test add and size methods of linkedList", function() {
+  it("should return size of linkedList", function() {
+    var linkedList = new eLinkedList();
+    linkedList.add('eStructures');
+    linkedList.add('linkedList');
+    linkedList.add('JavaScript');
+    expect(linkedList.size()).toBe(3);
+  });
+});
+
+describe("eLinkedList - test add and size methods of linkedList", function() {
+  it("should return size of linkedList after successfully adding item", function() {
+    var linkedList = new eLinkedList();
+    linkedList.add('eStructures');
+    linkedList.add('linkedList');
+    linkedList.add('JavaScript');
+    expect(linkedList.add({name: 'JavaScript'})).toBe(4);
+  });
+});
+
+describe("eLinkedList - test add method of linkedList", function() {
+  it("should return -1 if no parameter is passed into the add method", function() {
+    var linkedList = new eLinkedList();
+    linkedList.add('eStructures');
+    linkedList.add('linkedList');
+    linkedList.add('JavaScript');
+    expect(linkedList.add()).toBe(-1);
+  });
+});
+
+describe("eLinkedList - test getFirst method of linkedList", function() {
+  it("should return first item added to linkedList", function() {
+    var linkedList = new eLinkedList();
+    linkedList.add('eStructures');
+    linkedList.add('linkedList');
+    linkedList.add('JavaScript');
+    linkedList.add(57);
+    expect(linkedList.getFirst().data).toBe('eStructures');
+  });
+});
+
+describe("eLinkedList - test getLast method of linkedList", function() {
+  it("should return last item added to linkedList", function() {
+    var linkedList = new eLinkedList();
+    linkedList.add('eStructures');
+    linkedList.add('linkedList');
+    linkedList.add('JavaScript');
+    linkedList.add(57);
+    expect(linkedList.getLast().data).toBe(57);
+  });
+});
